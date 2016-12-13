a335650ed719af0a35475c243370983ccee3e746
Dmytro Roshchupkin <dmytro.roshchupkin@grammarly.com>
2016-12-13 13:09:46 +0200

    1

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// Load the http module to create an http server.
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+// requiring the HTTP interfaces in node 
+var http = require('http'); 
+var port = process.env.PORT || 8080
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/plain'});   
+  res.end('Hello World\n'); 
+}).listen(port);
