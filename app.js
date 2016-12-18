1be96ae865f0cc4ec830ccb3c45b381396b7c3b5
Dmytro Roshchupkin <dmytro.roshchupkin@grammarly.com>
2016-12-18 20:39:38 +0200

    1

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var http = require('http');
 var port = process.env.PORT || 80
 http.createServer(function (req, res) { 
   res.writeHead(200, {'Content-Type': 'text/plain'});   
-  res.end('Hello World\n'); 
+  res.end('Hello World f4\n'); 
 }).listen(port);
 
-var index = '
-<html><body>
-<h1>Here is a link</h1>
-<a href = "https://google.com">Google!</a>
-</body></html>
-'
+// var index = '
+// <html><body>
+// <h1>Here is a link</h1>
+// <a href = "https://google.com">Google!</a>
+// </body></html>
+// '
