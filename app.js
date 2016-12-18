d6eeb125fa689ab37a9f289deceac5a3405b358a
Dmytro Roshchupkin <dmytro.roshchupkin@grammarly.com>
2016-12-18 21:13:37 +0200

    1

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // requiring the HTTP interfaces in node 
 var http = require('http'); 
-var port = process.env.PORT || 80
+var port = process.env.PORT || 9000
 http.createServer(function (req, res) { 
   res.writeHead(200, {'Content-Type': 'text/plain'});   
   res.end('Hello World f4- from Travis\n'); 
 }).listen(port);
 
-// var index = '
-// <html><body>
-// <h1>Here is a link</h1>
-// <a href = "https://google.com">Google!</a>
-// </body></html>
-// '
+var index = '
+<html><body>
+<h1>Here is a link</h1>
+<a href = "https://google.com">Google!</a>
+</body></html>
+'
