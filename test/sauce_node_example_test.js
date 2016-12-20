4d5f1d8a5aa4b66d47dcfb1cb05db8a79797bf9a
Dmytro Roshchupkin <dmytro.roshchupkin@grammarly.com>
2016-12-20 12:59:23 +0200

    1

--- test/sauce_node_example_test.js
+++ test/sauce_node_example_test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 var should = require('should');
 
 var branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH
-var branchUrl = 'http://ci-workshop-test-1-' + process.env.BRANCH + '.herokuapp.com'
+var branchUrl = 'http://ci-workshop-test-1-' + branchName + '.herokuapp.com'
 
 console.log('>>>>>>', branchName)
 console.log('>>>>>>', process.env.TRAVIS_PULL_REQUEST_BRANCH)
