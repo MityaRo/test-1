cd25670719dc079da1767320d03fdd3357a6f230
Dmytro Roshchupkin <dmytro.roshchupkin@grammarly.com>
2016-12-20 12:58:33 +0200

    1

--- test/sauce_node_example_test.js
+++ test/sauce_node_example_test.js
@@ -2,9 +2,11 @@ var client = require('webdriverio')
 var assert = require('assert')
 var should = require('should');
 
+var branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH
 var branchUrl = 'http://ci-workshop-test-1-' + process.env.BRANCH + '.herokuapp.com'
 
-console.log('>>>>>>', process.env.BRANCH)
+console.log('>>>>>>', branchName)
+console.log('>>>>>>', process.env.TRAVIS_PULL_REQUEST_BRANCH)
 console.log('>>>>>>', process.env.TRAVIS_BRANCH)
 
 // browser.init()
