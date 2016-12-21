917f205a02a22a9844bd9e95e5650a51d9a7849a
Dmytro Roshchupkin <dmytro.roshchupkin@grammarly.com>
2016-12-21 09:47:57 +0200

    1

--- test/example_test.js
+++ test/example_test.js
@@ -2,15 +2,14 @@ var client = require('webdriverio')
 var assert = require('assert')
 var should = require('should');
 
-var branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH
+var branchName = process.env.TRAVIS_BRANCH || 'http://myurl.com'
 var branchUrl = 'http://ci-workshop-test-1-' + branchName + '.herokuapp.com'
     
 describe('x', function() {
   describe('1', function() {
     it('Title', function() {
         browser.url(branchUrl)
-        browser.getTitle().should.equal('Home - My Site')
-        browser.getText('h1').should.equal('')
+        browser.getText('h1').should.equal('My Site')
     });
   });
 });
